81afef03d6d5ef683816dedea4f6344e5d022fa5
qlibtarrant <btarrant@qualitylogic.com>
2023-06-28 11:35:58 -0400

    Updated to test no images, all browsers and mobile

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('has title - visual comparison', async ({ page }) => {
-  await page.goto('https://getbootstrap.com');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Bootstrap/);
+test('no image site - visual comparison', async ({ page }) => {
+  await page.goto('http://www.blankwebsite.com');
 
   // Perform visual comparison
   await expect(page).toHaveScreenshot()
