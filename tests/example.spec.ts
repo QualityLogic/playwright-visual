d16953ef0901bc999c344b8321bb4769bc94e513
qlibtarrant <btarrant@qualitylogic.com>
2023-06-28 12:57:09 -0400

    Refactoring

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,6 +3,9 @@ import { test, expect } from '@playwright/test';
 test('no image site - visual comparison', async ({ page }) => {
   await page.goto('http://www.blankwebsite.com');
 
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Blank/);
+
   // Perform visual comparison
   await expect(page).toHaveScreenshot()
 });
