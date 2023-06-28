896eceaddbf22d09f90728a76cc618cb84bb745e
qlibtarrant <btarrant@qualitylogic.com>
2023-06-28 09:57:15 -0400

    Refactoring to test bootstrap site.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('has title - visual comparison', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://getbootstrap.com');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Bootstrap/);
 
   // Perform visual comparison
   await expect(page).toHaveScreenshot()
