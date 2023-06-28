5b05097f0746a6d28890101ee6e2ace2c459e24b
qlibtarrant <btarrant@qualitylogic.com>
2023-06-28 10:37:02 -0400

    Refactored to test bootstrap site across platforms

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,16 +5,6 @@ test('has title - visual comparison', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Bootstrap/);
-});
-
-test('get started link - visual comparison', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
 
   // Perform visual comparison
   await expect(page).toHaveScreenshot()
