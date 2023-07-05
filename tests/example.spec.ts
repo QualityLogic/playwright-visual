11052efbc0f871aa71c07addb20178d476e47170
qlibtarrant <btarrant@qualitylogic.com>
2023-07-05 12:43:31 -0400

    Reformatted test with multiple locators to hide multiple elements.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('Visual regression - Masking', async ({ page }) => {
-  await page.goto('https://www.nytimes.com');
+  await page.goto('https://www.netflix.com');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/The New York Times/);
+  await expect(page).toHaveTitle(/Netflix/);
 
   // Perform visual comparison
   await expect(page).toHaveScreenshot({
-    mask: [page.locator('//*[@id="app"]/div[1]/div/div'), page.locator('//*[@id="site-content"]/div/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/section/a/figure/div/div/video')]
+    fullPage: true,
+    mask: [page.locator('#appMountPoint > div > div > div > div > div > div > div.our-story-cards > div:nth-child(3) > div.default-ltr-cache-b2rltc.ejqbulh1 > div.default-ltr-cache-7cljuy.ejqbulh3 > div'), 
+    page.locator('#appMountPoint > div > div > div > div > div > div > div.our-story-cards > div:nth-child(4) > div.default-ltr-cache-51sor6.ejqbulh1 > div.default-ltr-cache-vgp0nn.ejqbulh3 > div'), 
+    page.locator('#appMountPoint > div > div > div > div > div > div > div.our-story-cards > div:nth-child(6) > div.default-ltr-cache-51sor6.ejqbulh1 > div.default-ltr-cache-vgp0nn.ejqbulh3 > div > div > div.default-ltr-cache-wmj6sw.ecjxv8n5')]
   })
 });
