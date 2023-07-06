36f931e4172320564126ace163972d186f2fd9ff
qlibtarrant <btarrant@qualitylogic.com>
2023-07-06 10:25:02 -0400

    Clean up

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -12,8 +12,3 @@ test('Visual regression - Masking', async ({ page }) => {
     mask: [page.getByRole('presentation')]
   })
 });
-
-
-//     mask: [page.locator('#appMountPoint > div > div > div > div > div > div > div.our-story-cards > div:nth-child(3) > div.default-ltr-cache-b2rltc.ejqbulh1 > div.default-ltr-cache-7cljuy.ejqbulh3 > div'), 
-//     page.locator('#appMountPoint > div > div > div > div > div > div > div.our-story-cards > div:nth-child(4) > div.default-ltr-cache-51sor6.ejqbulh1 > div.default-ltr-cache-vgp0nn.ejqbulh3 > div'), 
-//     page.locator('#appMountPoint > div > div > div > div > div > div > div.our-story-cards > div:nth-child(6) > div.default-ltr-cache-51sor6.ejqbulh1 > div.default-ltr-cache-vgp0nn.ejqbulh3 > div > div > div.default-ltr-cache-wmj6sw.ecjxv8n5')]
