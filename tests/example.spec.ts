7c268a3e5d1dd3d0bcf79d27a2f0b85201d09f19
qlibtarrant <btarrant@qualitylogic.com>
2023-07-07 15:03:37 -0400

    Added comments back

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('Visual regression - Masking', async ({ page }) => {
+  //Testing NetFlix
   await page.goto('https://www.netflix.com');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Netflix/);
-
-  // Perform visual comparison
+  
+  // Perform visual comparison, while excluding the dynamic elements
   await expect(page).toHaveScreenshot({
     fullPage: true,
     mask: [page.getByRole('presentation')]
